dd589523e36464610f23d40ffb2af492c87f4062
tuvshno <tuvshno@gmail.com>
2024-04-27 16:00:00 -0500

    Add middleware.go

---
+++ middleware.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type MyHandler struct{}
+
+func (mh *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Perfect!"))
+	return
+}
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := r.Header.Get("Authorization")
+		if header == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		authType := strings.Split(header, " ")
+		fmt.Println(authType)
+		if len(authType) != 2 || authType[0] != "Basic" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		credentials, err := base64.StdEncoding.DecodeString(authType[1])
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		if string(credentials) == "Open Sesame" {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func main() {
+	targetHandler := MyHandler{}
+	panic(http.ListenAndServe(":8090", AuthMiddleware(&targetHandler)))
+}
