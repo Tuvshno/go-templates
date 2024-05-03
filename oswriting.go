7b3e3bd781a3ad72e545d34c284e79ebc379e851
tuvshno <tuvshno@gmail.com>
2024-05-03 16:00:00 -0500

    Add oswriting.go

---
+++ oswriting.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	tmp := os.TempDir()
+	file, err := os.Create(tmp + "/myfile")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	msg := "Save the world with go!!!"
+
+	_, err = file.WriteString(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	positions := []int{4, 10, 20}
+	for _, i := range positions {
+		_, err := file.Seek(int64(i), 0)
+		if err != nil {
+			panic(err)
+		}
+		file.Write([]byte("X"))
+	}
+
+	file.Seek(0, 0)
+
+	result := make([]byte, len(msg))
+	_, err = file.Read(result)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", result)
+}
